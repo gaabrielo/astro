3a3ef42c08f67e5ce6a913ce8ebd30ba56f1b2d1
Gabriel Mendonca <gabrielmendonca@Gabriels-MacBook-Air.local>
2023-08-11 11:11:38 -0300

    Route config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
